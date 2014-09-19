ce4e2db2d0fc478cdaff2c49330779d3e42a401b
uumoss <uumoss@gmail.com>
2014-09-18 20:06:34 -0600

    added right bar button item that will change the background color

--- NavigationColors/ViewController.m
+++ NavigationColors/ViewController.m
@@ -16,8 +16,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"Color" style:UIBarButtonItemStylePlain target:self action:@selector(color:)];
 }
+
+- (void)color:(UIBarButtonItem *)sender{
+    if (self.view.backgroundColor == [UIColor whiteColor]){
+        self.view.backgroundColor = [UIColor blueColor];
+    } else {
+        self.view.backgroundColor = [UIColor whiteColor];
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
